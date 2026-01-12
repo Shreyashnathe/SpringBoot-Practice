08bbdd6638812e4272600d2f6417fc6f53653046
Shreyash Nathe <shreyashnathe2005@gmail.com>
2026-01-12 21:08:44 +0530

    Optimizing all the Operations

--- 7_ECom/src/main/java/com/myPackage/service/OrderService.java
+++ 7_ECom/src/main/java/com/myPackage/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.myPackage.service;
 
+import com.myPackage.model.Order;
 import com.myPackage.model.dto.OrderRequest;
 import com.myPackage.model.dto.OrderResponse;
 import com.myPackage.repo.OrderRepo;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
@@ -14,8 +16,13 @@ public class OrderService {
     @Autowired
     private OrderRepo orderRepo;
 
-    public OrderResponse placeOrder(OrderRequest orderRequest) {
-        return orderRepo.
+    public OrderResponse placeOrder(OrderRequest request) {
+        Order order = new Order();
+        String orderId = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+        order.setOrderId(orderId);
+        order.setCustomerName(request.customerName());
+        order.setEmail(request.email());
+        return null;
     }
 
     public List<OrderResponse> getAllOrderResponses() {
