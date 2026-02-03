a3107acf34d92f3fec9db8d1393bb854f543ac4e
Shreyash Nathe <shreyashnathe2005@gmail.com>
2026-02-03 15:03:32 +0530

    Securing my Project

---
+++ 10_SpringSecurity/src/main/java/com/myPackage/HomeController.java
@@ -0,0 +1,18 @@
+package com.myPackage;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "Welcome to Home Page";
+    }
+
+    @GetMapping("/about")
+    public String About(){
+        return "This is About Page";
+    }
+}
