5816909cfe16836f542316d15e948b476837430e
Shreyash Nathe <shreyashnathe2005@gmail.com>
2026-02-07 07:13:18 +0530

    Use CSRF for credentials

--- 10_SpringSecurity/src/main/java/com/myPackage/StudentController.java
+++ 10_SpringSecurity/src/main/java/com/myPackage/StudentController.java
@@ -1,20 +1,27 @@
 package com.myPackage;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StudentController {
 
-    List<Student> studentList = List.of(
-            new Student(1, "Alice", "Java"),
-            new Student(2, "Bob", "Python"),
-            new Student(3, "Charlie", "JavaScript")
-    );
+    List<Student> studentList=new ArrayList<>(List.of(
+            new Student(1,"Navin","Java"),
+            new Student(2,"Kiran","Blockchain")
+    ));
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
 
     @GetMapping("/students")
     public List<Student> getStudents() {
@@ -22,8 +29,7 @@ public class StudentController {
     }
 
     @PostMapping("/student")
-    public Student addStudent(Student student) {
+    public void addStudent(@RequestBody Student student) {
         studentList.add(student);
-        return student;
     }
 }
