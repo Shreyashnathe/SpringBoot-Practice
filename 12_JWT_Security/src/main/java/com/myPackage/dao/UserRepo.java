ee04c9fa82a635569b0345839a513272e92249e2
Shreyash Nathe <shreyashnathe2005@gmail.com>
2026-02-09 10:50:27 +0530

    Register and Login Uer

---
+++ 12_JWT_Security/src/main/java/com/myPackage/dao/UserRepo.java
@@ -0,0 +1,10 @@
+package com.myPackage.dao;
+import com.myPackage.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
